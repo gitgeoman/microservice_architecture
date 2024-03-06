56a7c425cff4887697a90678b76549e12c00b51b
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2024-03-06 15:50:15 +0100

    wip: update server and service

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,4 +1,4 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
 
 CREATE DATABASE auth;
 
@@ -6,10 +6,11 @@ GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 
 USE auth;
 
-CREATE TABLE user(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
 );
+
 
 INSERT INTO user (email, password) VALUES ('marek@email.com','Admin123')
