f1af7b7fbc2a16e9809081c1772eddcc56a643c3
Marek <marek.wyszynski@bitapps.fi>
2024-03-18 22:24:04 +0100

    wip: add loging err condition

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -18,3 +18,8 @@ channel = connection.channel()
 @server.route('/login', methods=['POST'])
 def login():
     token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
