754362597916d907f5b2a7a01adda4d7d1aa59f1
Marek <marek.wyszynski@bitapps.fi>
2024-03-18 22:20:12 +0100

    wip: add basic auth and request post

--- python/src/gateway/auth_svc/access.py
+++ python/src/gateway/auth_svc/access.py
@@ -4,3 +4,7 @@ def login(request):
     auth = request.authorization
     if not auth:
         return None, ("missing credentials", 401)
+
+    basicAuth = (auth.username, auth.password)
+
+    response = requests.post('')
